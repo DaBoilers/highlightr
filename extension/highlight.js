cc40168388710c466e24c422c8695979b3b9b087
Harrison McCarty <mccarth@purdue.edu>
2022-01-22 23:59:52 -0500

    added final changes

--- extension/highlight.js
+++ extension/highlight.js
@@ -2,11 +2,48 @@ var text = "";
 
 if( document.readyState !== 'loading' ) {
   text = document.body.innerText;
-  sendToApi(text)
+
+  text = text.replace(/[^0-9a-zA-Z .]/gi, '')
+  const request = new Request("http://localhost:5000/ranked", {
+    headers: {'Content-Type': 'application/json'},
+    method: "POST",
+    body: '{"content": "' + text + '"}',
+  });
+
+  fetch(request)
+    .then((response) => {
+      if (response.status === 200) {
+        response.json()
+          .then((json) => {
+            for (const sentence of json["sentences"]) {
+              var elements = document.getElementsByTagName('paragraph')
+              console.log(elements)
+              for (var i = 0; i < elements.length; i++) {
+                  console.log(sentence)
+                  console.log(elements[i].innerHTML)
+                  if (elements[i].innerHTML.indexOf(sentence) !== -1) {
+                      alert('Match');
+                      break;
+                  }
+              }
+            }
+          });
+      } else {
+        throw new Error("Failed to fetch ranked sentences")
+      }
+    });
+  
 } else {
   document.addEventListener('DOMContentLoaded', async function () {
     text = document.body.innerText;
-    sendToApi(text)
+    let sentences = sendToApi(text);
+    for (sentence in sentences) {
+      var xpath = "//a[text()='" + sentence + "']";
+      console.log(xpath);
+      var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      console.log(matchingElement);
+      matchingElement.style.backgroundColor = "#FDFF47";
+    }
   });
 }
 
@@ -19,19 +56,15 @@ function sendToApi(text) {
     body: '{"content": "' + text + '"}',
   });
 
-  fetch(request)
+  return fetch(request)
     .then((response) => {
       if (response.status === 200) {
-        response.json()
-          .then((data) => {
-            console.log(data)
+        return response.json()
+          .then((sentences) => {
+            return sentences
           });
       } else {
         throw new Error("Failed to fetch ranked sentences")
       }
     });
 }
-
-// Search for returned sentences
-
-// Add <mark> tag to returned sentences
