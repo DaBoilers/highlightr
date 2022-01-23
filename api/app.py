67086c71a3df33ec0262af8cebc1a9628f821fa7
katyayego <katya.yego@gmail.com>
2022-01-22 21:17:53 -0500

    undo all deploy

---
+++ api/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from articleparser import ArticleParser
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+p = ArticleParser()
+
+@app.route("/")
+def index():
+    return jsonify({ 'status' : 'running'})
+
+@app.route('/ranked', methods=['POST'])
+def ranked():
+    json = request.get_json(force=True, silent=True)
+    if json is not None and 'content' in json:
+        return {
+            "sentences": p.get_ranked_sentences(json['content'])
+        }
+    else:
+        return {
+            "error": "No content provided"
+        }
+
+
